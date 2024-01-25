9aafec72f2fb797a56df944a89fd82a33ed6fd7a
Vandern Rodrigues <nrednav@gmail.com>
2024-01-25 01:55:20 +0000

    feat: add & setup basic web-app (#3)
    - feat: add & setup basic react +vite web-app
    - feat: extend ESLint configuration
    - refactor: rename title of web-app
    - refactor: remove template code + assets
    - feat: define global stylesheet in index.css
    - feat: add & setup tailwindcss
    - feat: add & setup shadcn/ui
      - update some of the recommended ESLint rules
      - update tsconfig.json

---
+++ src/web-app/.eslintrc.cjs
@@ -0,0 +1,38 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/strict-type-checked",
+    "plugin:@typescript-eslint/stylistic-type-checked",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+  ],
+  ignorePatterns: [
+    "build",
+    ".eslintrc.cjs",
+    "postcss.config.cjs",
+    "tailwind.config.ts",
+    "vite.config.ts",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "script",
+    project: ["./tsconfig.json", "./tsconfig.node.json"],
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ["react-refresh"],
+  rules: {
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
+    ],
+    "@typescript-eslint/no-confusing-void-expression": [
+      "error",
+      { ignoreArrowShorthand: true },
+    ],
+    "@typescript-eslint/consistent-type-definitions": ["error", "type"],
+  },
+};
