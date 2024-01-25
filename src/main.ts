4e005d3cdf959f4c6b13b1f7730de07798217bc7
Vandern Rodrigues <nrednav@gmail.com>
2024-01-25 03:05:43 +0000

    feat: add & setup yargs + basic workflow

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,31 @@
-console.log("npm-audit-visualizer");
+#!/usr/bin/env node
+
+import { exec } from "child_process";
+import yargs from "yargs";
+
+const argv = yargs
+  .option("run", {
+    describe: "Run the application",
+    type: "boolean",
+  })
+  .help()
+  .alias("help", "h")
+  .parseSync();
+
+const run = () => {
+  console.log("npm-audit-visualizer");
+
+  // Start the React application
+  exec("cd src/web-app && pnpm run dev", (error, stdout, stderr) => {
+    if (error) {
+      console.error(`exec error: ${error}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+    console.error(`stderr: ${stderr}`);
+  });
+};
+
+if (argv.run) {
+  run();
+}
