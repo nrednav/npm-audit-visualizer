37ef74678c0e4d6ea7b7ceb0c8158c1fed59c09d
Vandern Rodrigues <nrednav@gmail.com>
2024-01-25 03:32:40 +0000

    feat: add & setup yargs + basic workflow (#4)
    * add dependencies (yargs, @types/yargs, chalk, del-cli)
    * convert project to ESM
    * add dev:watch, build, clean & start scripts
    * setup basic yargs workflow in main.ts
    * update tsconfig.json to reflect changes required by scripts

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,33 @@
-console.log("npm-audit-visualizer");
+#!/usr/bin/env node
+
+import { exec } from "node:child_process";
+import chalk from "chalk";
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+
+const argv = yargs(hideBin(process.argv))
+  .option("run", {
+    describe: "Run the application",
+    type: "boolean",
+  })
+  .help()
+  .alias("help", "h")
+  .parseSync();
+
+const run = () => {
+  console.log(chalk.green("npm-audit-visualizer"));
+
+  // Start the React application
+  exec("cd src/web-app && pnpm run dev", (error, stdout, stderr) => {
+    if (error) {
+      console.error(`exec error: ${error}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+    console.error(`stderr: ${stderr}`);
+  });
+};
+
+if (argv.run) {
+  run();
+}
