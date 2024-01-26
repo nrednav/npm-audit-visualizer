1e4eb24334b957357badae8f989d5db5d6fe10f4
Vandern Rodrigues <nrednav@gmail.com>
2024-01-26 01:34:53 +0000

    feat: setup testing (#5)
    * feat: add & setup vitest for backend
    * feat(web-app): add & setup vitest + react-testing-library for frontend
    * feat: add eslint-plugin-jest-dom & eslint-plugin-testing-library

--- src/web-app/vite.config.ts
+++ src/web-app/vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
@@ -11,6 +12,10 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  test: {
+    environment: "jsdom",
+    setupFiles: "./setupTests.ts",
   },
   plugins: [react()],
 });
