76c1dd2d73d68e80511dc2fc1d4f813829a07806
Vandern Rodrigues <nrednav@gmail.com>
2024-01-26 00:33:38 +0000

    feat: add & setup vitest for testing the console app

---
+++ vitest.config.ts
@@ -0,0 +1,10 @@
+/// <reference types="vitest" />
+import { configDefaults, defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    environment: "node",
+    exclude: [...configDefaults.exclude, "src/web-app/*"],
+    passWithNoTests: true,
+  },
+});
