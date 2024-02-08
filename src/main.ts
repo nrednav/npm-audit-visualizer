8060f97a4d0a1667e5403f3a9c5a37b1d2ca6771
Vandern Rodrigues <nrednav@gmail.com>
2024-02-08 01:23:24 +0000

    refactor: re-structure main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,33 +1,49 @@
 #!/usr/bin/env node
 
 import { exec } from "node:child_process";
+import { fileURLToPath } from "node:url";
 import chalk from "chalk";
 import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
 
-const argv = yargs(hideBin(process.argv))
-  .option("run", {
-    describe: "Run the application",
-    type: "boolean",
-  })
-  .help()
-  .alias("help", "h")
-  .parseSync();
-
-const run = () => {
+export const main = () => {
   console.log(chalk.green("npm-audit-visualizer"));
 
-  // Start the React application
-  exec("cd src/web-app && pnpm run dev", (error, stdout, stderr) => {
-    if (error) {
-      console.error(`exec error: ${error}`);
-      return;
-    }
-    console.log(`stdout: ${stdout}`);
-    console.error(`stderr: ${stderr}`);
-  });
+  const argv = yargs(hideBin(process.argv))
+    .options({
+      file: {
+        type: "string",
+        describe: 'Path to file containing output of "npm audit --json"',
+        alias: "f",
+        demandOption: true,
+      },
+      web: {
+        type: "boolean",
+        describe: "Runs the web app",
+        alias: "w",
+      },
+    })
+    .help()
+    .alias("help", "h")
+    .parseSync();
+
+  if (argv.file) {
+    console.log(`Audit File: ${chalk.blueBright(argv.file)}`);
+  }
+
+  if (argv.web) {
+    // Start the React application
+    exec("cd src/web-app && pnpm run dev", (error, stdout, stderr) => {
+      if (error) {
+        console.error(`exec error: ${error}`);
+        return;
+      }
+      console.log(`stdout: ${stdout}`);
+      console.error(`stderr: ${stderr}`);
+    });
+  }
 };
 
-if (argv.run) {
-  run();
+if (fileURLToPath(import.meta.url) === process.argv[1]) {
+  main();
 }
