c1836af33cdaa642b0e8ed38dda8ac778d08fd38
Vandern Rodrigues <nrednav@gmail.com>
2024-02-28 21:47:20 +0000

    feat(web-app): define base layout + add header

--- web-app/src/App.tsx
+++ web-app/src/App.tsx
@@ -1,5 +1,13 @@
 const App = () => {
-  return <div className="text-2xl font-bold">App</div>;
+  return (
+    <>
+      <header className="debug-r">
+        <h1 className="text-2xl font-bold text-center">npm-audit-visualizer</h1>
+      </header>
+      <main />
+      <footer />
+    </>
+  );
 };
 
 export default App;
