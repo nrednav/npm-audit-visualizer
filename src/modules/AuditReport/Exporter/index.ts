cccad2246ee78581b212a466ffd9f2b7a6e4bbe7
Vandern Rodrigues <nrednav@gmail.com>
2024-03-16 01:50:56 +0000

    feat: implement audit report exporter as a TaskEither

--- src/modules/AuditReport/Exporter/index.ts
+++ src/modules/AuditReport/Exporter/index.ts
@@ -1,1 +1,43 @@
-export const exportParsedAuditReport = () => {};
+import { writeFile } from "node:fs";
+import path from "node:path";
+import { promisify } from "node:util";
+import * as TE from "fp-ts/lib/TaskEither.js";
+import { ParsedAuditReport } from "src/modules/AuditReport/Parser/types.js";
+import { AppError } from "src/shared/errors.js";
+import { logger } from "src/shared/modules/logger.js";
+import { assertIsError } from "src/shared/utils.js";
+
+const writeFileAsync = promisify(writeFile);
+
+export const exportParsedAuditReport = (
+  parsedAuditReport: ParsedAuditReport,
+): TE.TaskEither<AppError, ParsedAuditReport> => {
+  logger.debug("Exporting parsed audit report");
+
+  const serializedReport = JSON.stringify(parsedAuditReport, null, 2);
+  const filename = "parsed-audit-report.json";
+  const outputPath = path.resolve(
+    process.cwd(),
+    "src/modules/WebApp/src/data",
+    filename,
+  );
+
+  return TE.tryCatchK(
+    async () => {
+      await writeFileAsync(outputPath, serializedReport, "utf-8");
+      return parsedAuditReport;
+    },
+    (error: unknown) => {
+      assertIsError(error);
+      return new AppError(
+        "Failed to export parsed audit report",
+        {
+          file: "modules/AuditReport/Exporter/index.ts",
+          functionName: "exportParsedAuditReport",
+          data: { parsedAuditReport },
+        },
+        error,
+      );
+    },
+  )();
+};
