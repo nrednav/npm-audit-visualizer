d59b3b1d5eaa26be11b5531d96a424c88621b7e6
Vandern Rodrigues <nrednav@gmail.com>
2024-04-14 21:29:09 +0100

    add console.error log to importParsedAuditReport function

--- src/modules/WebApp/src/modules/audit-report.ts
+++ src/modules/WebApp/src/modules/audit-report.ts
@@ -3,6 +3,8 @@ import {
   ParsedAuditReportSchema,
 } from "root/src/modules/AuditReport/Parser/types";
 
+import { assertIsError } from "root/src/shared/utils";
+
 export const importParsedAuditReport =
   async (): Promise<ParsedAuditReport | null> => {
     try {
@@ -10,6 +12,8 @@ export const importParsedAuditReport =
       const parsedAuditReport = (await res.json()) as unknown;
       return ParsedAuditReportSchema.parse(parsedAuditReport);
     } catch (error) {
+      assertIsError(error);
+      console.error(`Failed to import parsed audit report: \n${error.message}`);
       return null;
     }
   };
