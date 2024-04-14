9b59ebecb3d38cb5d58070b844cf0f8a8513dd39
Vandern Rodrigues <nrednav@gmail.com>
2024-04-14 16:44:54 +0100

    define light & dark colors in palette for web-app

--- src/modules/WebApp/tailwind.config.ts
+++ src/modules/WebApp/tailwind.config.ts
@@ -4,7 +4,12 @@ const config = {
   darkMode: ["class"],
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        light: "#f2f4f6",
+        dark: "#162026",
+      },
+    },
   },
   plugins: [],
 } satisfies TailwindConfig;
