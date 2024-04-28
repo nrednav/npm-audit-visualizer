5912a70177ddfa57d16a11f3cb367ad704a76221
Vandern Rodrigues <nrednav@gmail.com>
2024-04-29 00:00:07 +0100

    refactor edge & node reducers into separate functions

--- src/modules/WebApp/src/components/Visualizer.tsx
+++ src/modules/WebApp/src/components/Visualizer.tsx
@@ -18,7 +18,11 @@ import {
 import "@react-sigma/core/lib/react-sigma.min.css";
 import { MultiDirectedGraph } from "graphology";
 import { circular } from "graphology-layout";
+import { Attributes } from "graphology-types";
+import { Sigma } from "sigma";
+import { NodeDisplayData } from "sigma/types";
 import { COLORS } from "src/constants";
+import { Vulnerability } from "../../../AuditReport/Validator/schema";
 
 type VisualizerProps = {
   parsedAuditReport: ParsedAuditReport;
@@ -100,76 +104,92 @@ export const VulnerabilityGraphComponent = ({
 
   useEffect(() => {
     setSettings({
-      nodeReducer: (node, nodeAttributes) => {
-        const graph = sigma.getGraph();
-
-        const newNodeAttributes: { highlighted: boolean; color?: string } = {
-          ...nodeAttributes,
-          highlighted: Boolean(nodeAttributes.highlighted) || false,
-        };
-
-        if (
-          hoveredNode &&
-          (hoveredNode === node || graph.neighbors(hoveredNode).includes(node))
-        ) {
-          newNodeAttributes.highlighted = true;
-          newNodeAttributes.size = nodeAttributes.size * 2;
-
-          if (nodeAttributes.vulnerability.via.includes(hoveredNode)) {
-            newNodeAttributes.color = COLORS.indigo;
-          } else {
-            newNodeAttributes.color = COLORS.coral;
-          }
-
-          if (hoveredNode === node) {
-            newNodeAttributes.color = COLORS.gold;
-          }
-
-          for (const outboundEdge of graph.outboundEdges(hoveredNode)) {
-            graph.setEdgeAttribute(outboundEdge, "color", COLORS.coral);
-          }
-
-          for (const inboundEdge of graph.inboundEdges(hoveredNode)) {
-            graph.setEdgeAttribute(inboundEdge, "color", COLORS.indigo);
-          }
-        } else {
-          newNodeAttributes.highlighted = false;
-          newNodeAttributes.color = COLORS.black;
-
-          if (previouslyHoveredNode) {
-            for (const outboundEdge of graph.outboundEdges(
-              previouslyHoveredNode,
-            )) {
-              graph.setEdgeAttribute(outboundEdge, "color", COLORS.black);
-            }
-
-            for (const inboundEdge of graph.inboundEdges(
-              previouslyHoveredNode,
-            )) {
-              graph.setEdgeAttribute(inboundEdge, "color", COLORS.black);
-            }
-          }
-        }
-
-        return newNodeAttributes;
-      },
-      edgeReducer: (edge, data) => {
-        const graph = sigma.getGraph();
-        const newEdgeAttributes: { hidden: boolean; color?: string } = {
-          ...data,
-          hidden: false,
-        };
-
-        if (hoveredNode && !graph.extremities(edge).includes(hoveredNode)) {
-          newEdgeAttributes.hidden = true;
-        }
-
-        return newEdgeAttributes;
-      },
+      nodeReducer: nodeReducer({ sigma, hoveredNode, previouslyHoveredNode }),
+      edgeReducer: edgeReducer({ sigma, hoveredNode }),
     });
   }, [hoveredNode, setSettings, sigma, previouslyHoveredNode]);
 
   return null;
 };
+
+type NodeReducerContext = {
+  sigma: Sigma;
+  hoveredNode: string | null;
+  previouslyHoveredNode: string | null;
+};
+
+const nodeReducer =
+  (context: NodeReducerContext) =>
+  (node: string, data: Attributes): Partial<NodeDisplayData> => {
+    const { sigma, hoveredNode, previouslyHoveredNode } = context;
+
+    const graph = sigma.getGraph();
+
+    const newNodeData: Attributes = {
+      ...data,
+      highlighted: Boolean(data.highlighted) || false,
+    };
+
+    if (hoveredNode && node === hoveredNode) {
+      newNodeData.highlighted = true;
+      newNodeData.size = data.size * 2;
+      newNodeData.color = COLORS.gold;
+
+      for (const outboundEdge of graph.outboundEdges(hoveredNode)) {
+        graph.setEdgeAttribute(outboundEdge, "color", COLORS.coral);
+      }
+
+      for (const inboundEdge of graph.inboundEdges(hoveredNode)) {
+        graph.setEdgeAttribute(inboundEdge, "color", COLORS.indigo);
+      }
+    } else if (hoveredNode && graph.neighbors(hoveredNode).includes(node)) {
+      newNodeData.highlighted = true;
+      newNodeData.size = data.size * 2;
+      newNodeData.color = getNeighboringNodeColor(data, hoveredNode);
+    } else {
+      newNodeData.highlighted = false;
+      newNodeData.color = COLORS.black;
+
+      if (previouslyHoveredNode) {
+        for (const outboundEdge of graph.outboundEdges(previouslyHoveredNode)) {
+          graph.setEdgeAttribute(outboundEdge, "color", COLORS.black);
+        }
+
+        for (const inboundEdge of graph.inboundEdges(previouslyHoveredNode)) {
+          graph.setEdgeAttribute(inboundEdge, "color", COLORS.black);
+        }
+      }
+    }
+
+    return newNodeData;
+  };
+
+const getNeighboringNodeColor = (data: Attributes, hoveredNode: string) => {
+  const vulnerability = data.vulnerability as unknown as Vulnerability;
+
+  if (vulnerability.via.includes(hoveredNode)) {
+    return COLORS.indigo;
+  }
+
+  return COLORS.coral;
+};
+
+type EdgeReducerContext = {
+  sigma: Sigma;
+  hoveredNode: string | null;
+};
+
+const edgeReducer =
+  (context: EdgeReducerContext) => (edge: string, data: Attributes) => {
+    const { sigma, hoveredNode } = context;
+    const graph = sigma.getGraph();
+    const newEdgeData: Attributes = { ...data, hidden: false };
+
+    if (hoveredNode && !graph.extremities(edge).includes(hoveredNode)) {
+      newEdgeData.hidden = true;
+    }
+
+    return newEdgeData;
+  };
 
 export default Visualizer;
