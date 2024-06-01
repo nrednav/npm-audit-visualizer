b9865fca62b17a1565ae1c7ba94a5dd91936f136
Vandern Rodrigues <nrednav@gmail.com>
2024-06-01 22:47:59 +0100

    update styles of metadata cards

--- src/modules/WebApp/src/components/AuditReportMetadata.tsx
+++ src/modules/WebApp/src/components/AuditReportMetadata.tsx
@@ -1,5 +1,7 @@
+import clsx from "clsx";
 import { useState } from "react";
 import type { ParsedAuditReport } from "root/src/modules/AuditReport/Parser/types";
+import { SEVERITY_COLOR_MAP } from "src/constants";
 import { TabList } from "./TabList";
 
 type AuditReportMetadataProps = {
@@ -34,7 +36,7 @@ const MetadataCardList = (props: MetadataCardListProps) => {
   const { data } = props;
   const keys = Object.keys(data);
   return (
-    <ul className="flex flex-row gap-4">
+    <ul className="py-4 | flex flex-row gap-4">
       {keys.map((key) => {
         const value = String(data[key]);
         return (
@@ -53,9 +55,18 @@ type MetadataCardProps = {
 const MetadataCard = (props: MetadataCardProps) => {
   const { label, value } = props;
   return (
-    <li className="flex flex-col">
-      <p>{label}</p>
-      <p>{value}</p>
+    <li
+      className={clsx(
+        "w-32 p-4 | flex flex-col | rounded-lg bg-white shadow border-r-4",
+      )}
+      style={{ borderRightColor: SEVERITY_COLOR_MAP[label.toLowerCase()] }}
+    >
+      <p className="truncate text-sm font-semibold text-slate-500 capitalize">
+        {label}
+      </p>
+      <p className="mt-1 text-3xl font-semibold tracking-tight text-dark">
+        {value}
+      </p>
     </li>
   );
 };
