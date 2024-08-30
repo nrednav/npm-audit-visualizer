184308e0738d17d4487bd50c38634af1ff2d7055
Vandern Rodrigues <nrednav@gmail.com>
2024-08-31 00:59:52 +0100

    vertical spacing

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -4,10 +4,12 @@ import path from "path";
 const require = createRequire(import.meta.url);
 
 export const PACKAGE_NAME = "npm-audit-visualizer";
+
 export const PACKAGE_ENTRY_POINT = {
   relative: "build/main.js",
   absolute: require.resolve(PACKAGE_NAME),
 };
+
 const PACKAGE_BUILD_DIR = path.dirname(PACKAGE_ENTRY_POINT.absolute);
 
 export const WEB_APP_BUILD_DIR = path.resolve(PACKAGE_BUILD_DIR, "web-app");
