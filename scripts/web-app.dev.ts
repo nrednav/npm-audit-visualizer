7cbedbb7b62e0e185a1bfcbecffa7c022760e3c8
Vandern Rodrigues <nrednav@gmail.com>
2024-08-31 19:18:29 +0100

    add error handling to web-app.dev script

--- scripts/web-app.dev.ts
+++ scripts/web-app.dev.ts
@@ -3,16 +3,20 @@ import { execSync } from "child_process";
 const run = () => {
   console.log("Running web-app dev script...");
 
-  const [auditReportFilePath] = process.argv.slice(2);
-
-  const command = [
-    `npm run dev -- --export-only -f ${auditReportFilePath}`,
-    "npm --prefix src/modules/web-app run dev",
-  ].join(" && ");
-
-  execSync(command, { stdio: "inherit" });
-
   try {
+    const [auditReportFilePath] = process.argv.slice(2);
+
+    if (!auditReportFilePath) {
+      throw new Error("No audit report file provided");
+    }
+
+    const command = [
+      `npm run dev -- --export-only -f ${auditReportFilePath}`,
+      "npm --prefix src/modules/web-app run dev",
+    ].join(" && ");
+
+    execSync(command, { stdio: "inherit" });
+
     console.log("Finished running web-app dev script.");
   } catch (error) {
     console.error(error);
